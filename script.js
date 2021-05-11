3c99cbfc60135cda6642a6515b98a348529f89d1
maurcord <67590886+maurcord@users.noreply.github.com>
2021-05-11 00:55:15 -0400

    added gladiator options to arena

--- script.js
+++ script.js
@@ -5,36 +5,39 @@ class Gladiator {
         this.weapon = weapon
     }
 }
+
 class Arena {
     constructor(name, gladiators){
         this.name = name.charAt(0).toUpperCase() + name.slice(1)
         this.gladiators = []
-    }
-
-
-addGladiator(gladiator){
-    if(this.gladiators.length < 2) {
+ } 
+  
+    addGladiator(gladiator){
+      if(this.gladiators.length < 2) {
         this.gladiators.push(gladiator)
 
-        function fight () {
-            console.log('Fight')
-        }
-fight()
-}   
-}
-}
+    }}}
 
-
+console.log()
 const max = new Gladiator("Maximus", "Trident");
 const troy = new Gladiator("Troy", "Spear");
 const hector = new Gladiator("Hector", "Club");
 
 const colosseum = new Arena("colosseum");
-console.log(colosseum.name)
+// console.log(colosseum.name)
 
 
 colosseum.addGladiator(max);
-console.log(colosseum.gladiators)
+// console.log(colosseum.gladiators)
 colosseum.addGladiator(troy);
-console.log(colosseum.gladiators[0].name)
-console.log(colosseum.gladiators[1].name)
+// console.log(colosseum.gladiators[0].name)
+// console.log(colosseum.gladiators[0].name)
+colosseum.addGladiator(hector);
+
+
+
+
+
+
+
+
