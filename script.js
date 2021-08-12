ba1e9496b7e46bc1c622db86699886fa1e60c601
maurcord <67590886+maurcord@users.noreply.github.com>
2021-08-11 23:48:28 -0400

    created addGladiator and fight methods

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
+    
 
 class Gladiator {
     constructor(name, weapon){
-        this.name = name
-        this.weapon = weapon
+        this.name = name;
+        this.weapon = weapon;
     }
 }
 
 class Arena {
     constructor(name, gladiators){
         this.name = name.charAt(0).toUpperCase() + name.slice(1)
-        this.gladiators = []
- } 
+        this.gladiators = []  
+    } 
   
     addGladiator(gladiator){
-      if(this.gladiators.length < 2) {
-        this.gladiators.push(gladiator)
+            this.gladiators.push(gladiator)
+            var arenaFighters = this.gladiators.sort(function(){return 0.5 - Math.random()});
+                if(this.gladiators.length < 2) {
+                console.log( "There are two fighters in the arena!")
+            }
+        }
+        fight() {
+        console.log("There is a battle in the", this.name, "." , this.gladiators[0].name, " & " , this.gladiators[1].name , " are in the arena!" )
 
-    }}}
+        if(this.gladiators[0].name == "Troy" && this.gladiators[1].name == "Maximus"){
+            console.log("Troy is fighter 1", "and Maximus is fighter 2! Maximus defeats Troy with his mighty Trident!")
+        } 
+        if(this.gladiators[0].name == "Hector" && this.gladiators[1].name == "Maximus"){
+            console.log("Hector is fighter 1", "and Maximus is fighter 2! Hector mutilates Maximus with his mighty club!")
+        }
+        if(this.gladiators[0].name == "Troy" && this.gladiators[1].name == "Hector"){
+            console.log("Troy is fighter 1", "and Hector is fighter 2! Troy defeats Hector with his super sharp spear!")
+    }   i
+        }
+}
 
-console.log()
-const max = new Gladiator("Maximus", "Trident");
-const troy = new Gladiator("Troy", "Spear");
-const hector = new Gladiator("Hector", "Club");
 
-const colosseum = new Arena("colosseum");
-// console.log(colosseum.name)
 
+
+var max = new Gladiator("Maximus", "Trident");
+var troy = new Gladiator("Troy", "Spear");
+var hector = new Gladiator("Hector", "Club");
+var colosseum = new Arena("colosseum");
 
 colosseum.addGladiator(max);
-// console.log(colosseum.gladiators)
 colosseum.addGladiator(troy);
-// console.log(colosseum.gladiators[0].name)
-// console.log(colosseum.gladiators[0].name)
 colosseum.addGladiator(hector);
+
+colosseum.fight()
+
+
+
 
 
 
